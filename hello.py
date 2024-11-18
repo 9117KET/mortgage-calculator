3f66481a4e02c15c9463d4675b73dcdf28fcccf6
Kinlo Ephriam Tangiri (KET) <kinlotangiri911@gmail.com>
2024-11-18 07:02:57 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.write("Hello World")
+x = st.text_input("Favourite movie?")
+
+st.write(f"Your favourite movie is: {x}")
+
+data = pd.read_csv("movies.csv")
+st.write(data)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),
+    columns=["a", "b", "c"]
+)
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
